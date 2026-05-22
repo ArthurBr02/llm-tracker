c2359aece228d9fe5b262bce3471f55d85a6b6b6
Arthur <arthurbratigny@gmail.com>
2026-05-22 11:16:50 +0200

    add prints

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import lmstudio
 
 
 if __name__ == "__main__":
+    print("Checking for new models...")
+    print("----------------------------------")
+    print("OpenRouter:")
     openrouter.execute()
+    print("----------------------------------")
+    print("Ollama:")
     ollama.execute()
+    print("----------------------------------")
+    print("LM Studio:")
     lmstudio.execute()
